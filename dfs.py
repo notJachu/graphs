22309777b8e544f6e6b2011b100b3ef2be49013f
notJachu <jasiustasiu50@interia.pl>
2023-04-16 23:39:20 +0200

    bttr dfs

--- dfs.py
+++ dfs.py
@@ -7,58 +7,59 @@ from collections import defaultdict
 # adjacency list representation
 
 
+# An Iterative Python program to do DFS traversal from
+# a given source vertex. DFS(int s) traverses vertices
+# reachable from s.
+
+# This class represents a directed graph using adjacency
+# list representation
 class Graph:
+    def __init__(self, V):  # Constructor
+        self.V = V  # No. of vertices
+        self.adj = [[] for i in range(V)]  # adjacency lists
 
-    # Constructor
-    def __init__(self):
+    def addEdge(self, v, w):  # to add an edge to graph
+        self.adj[v].append(w)  # Add w to v’s list.
 
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
+    # prints all not yet visited vertices reachable from s
+    def DFS(self, s):  # prints all vertices in DFS manner from a given source.
+        # Initially mark all vertices as not visited
+        visited = [False for i in range(self.V)]
 
-    # function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
+        # Create a stack for DFS
+        stack = []
 
-    # A function used by DFS
-    def DFSUtil(self, v, visited):
+        # Push the current source node.
+        stack.append(s)
 
-        # Mark the current node as visited
-        # and print it
-        visited.add(v)
-        print(v, end=' ')
+        while (len(stack)):
+            # Pop a vertex from stack and print it
+            s = stack[-1]
+            stack.pop()
 
-        # Recur for all the vertices
-        # adjacent to this vertex
-        for neighbour in self.graph[v]:
-            if neighbour not in visited:
-                self.DFSUtil(neighbour, visited)
+            # Stack may contain same vertex twice. So
+            # we need to print the popped item only
+            # if it is not visited.
+            if (not visited[s]):
+                print(s, end=' ')
+                visited[s] = True
 
-    # The function to do DFS traversal. It uses
-    # recursive DFSUtil()
-    def DFS(self, v):
-
-        # Create a set to store visited vertices
-        visited = set()
-
-        # Call the recursive helper function
-        # to print DFS traversal
-        self.DFSUtil(v, visited)
-
-
-# Driver's code
+            # Get all adjacent vertices of the popped vertex s
+            # If a adjacent has not been visited, then push it
+            # to the stack.
+            for node in self.adj[s]:
+                if (not visited[node]):
+                    stack.append(node)
 
 
-# Create a graph given
-# in the above diagram
-if __name__ == "__main__":
-    g = Graph()
-    g.addEdge(0, 1)
-    g.addEdge(0, 2)
-    g.addEdge(1, 2)
-    g.addEdge(2, 0)
-    g.addEdge(2, 3)
-    g.addEdge(3, 3)
+# Driver program to test methods of graph class
 
-    print("Following is DFS from (starting from vertex 2)")
-    # Function call
-    g.DFS(2)
+g = Graph(5);  # Total 5 vertices in graph
+g.addEdge(1, 0);
+g.addEdge(0, 2);
+g.addEdge(2, 1);
+g.addEdge(0, 3);
+g.addEdge(1, 4);
+
+print("Following is Depth First Traversal")
+g.DFS(0)
