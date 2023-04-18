20dd3b3dbe40252aa44d1492abed749e207ac85b
notJachu <jasiustasiu50@interia.pl>
2023-04-18 20:56:50 +0200

    finds path :)

--- main.py
+++ main.py
@@ -5,75 +5,84 @@ with open('./labirynt.txt') as plik:
     for a in range(20):
         maze[a] = list(maze[a].rstrip('\n'))
 
-    visited = [[False] * 20] * 20
+    visited = []
+
+    for i in range(20):
+        tmp = []
+        for j in range(20):
+            tmp.append(False)
+        visited.append(tmp)
 
 
 def getMoves(x, y):
     moves = []
-    for i in range(-1, 1):
-        if x + i >= 0 and y - 1 >= 0:
-            #print(i)
+    for i in range(-1, 2):
+        if 0 <= x + i < 20 and y - 1 >= 0:
+            # print(i)
             if maze[x + i][y - 1] != '#' and not visited[x + i][y - 1]:
                 tmp = [x + i, y - 1]
                 # print(i)
                 moves.append(tmp)
 
-    for i in range(-1, 1):
-        if x + i >= 0 and y + 1 <= 20:
-            #print(i)
+    for i in range(-1, 2):
+
+        if 0 <= x + i < 20 and y + 1 < 20:
+            # print(i)
             if maze[x + i][y + 1] != '#' and not visited[x + i][y + 1]:
                 tmp = [x + i, y + 1]
                 # print(i)
                 moves.append(tmp)
 
-    if x - 1 >= 0:
+    if 0 <= x - 1 < 20:
         if maze[x - 1][y] != '#' and not visited[x - 1][y]:
             tmp = [x - 1, y]
             moves.append(tmp)
 
-    if x + 1 >= 0:
-        if maze[x + 1][y] != '#' and not visited[x + 1][y]:
+    if 0 <= x + 1 < 20:
+        if maze[y][x + 1] != '#' and not visited[x + 1][y]:
             tmp = [x + 1, y]
             moves.append(tmp)
 
-    #print(moves)
+    print("valid moves", moves)
     return moves
 
 
 def solve(sx, sy, gx, gy):
     q = queue.Queue()
-    q.put([sx, sy])
+    q.put([[sx, sy]])
 
-    newPath = []
+    while not q.empty():
 
-    while q.qsize() > 0:
+        s = q.get()
+        print("entire path ", s)
+        print("last node ", s[-1])
+        lastNode = s[-1]
 
-        newPath.append(q.get())
-        path = newPath
+        visited[lastNode[0]][lastNode[1]] = True
 
-        print(path)
-        sx = path[len(path) - 1][0]
-        sy = path[len(path) - 1][1]
-        if sx == gx and sy == gy:
-            return path
-        moves = getMoves(sx, sy)
-        visited[sx][sy] = True
-        for m in moves:
-            #print(m)
-            newPath.append(m)
-            print(newPath)
-            print(q.put(newPath))
-            print(newPath)
+        valid = getMoves(lastNode[0], lastNode[1])
 
-
-        #    q.put(newPath)
+        for v in valid:
+            if v[0] == gx - 1 and v[1] == gy - 1:
+                print("PATH TO EXIT", s)
+                return s
+            p = []
+            for k in s:
+                p.append(k)
+            p.append(v)
+            print("new q", p)
+            print("old q", s)
+            q.put(p)
+            #print("new q", s)
 
 
 if __name__ == '__main__':
     for a in maze:
         print(a)
+    # visited[0][1] = True
     for a in visited:
         print(a)
     print(maze[0][6])
     solve(0, 0, 20, 20)
+    #getMoves(0, 1)
     # print(visited)
